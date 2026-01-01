8dc49fb1286d2602c0e734027414149ba3ad3c67
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-01-01 15:04:57 -0500

    add flake.nix stuff for dev shells
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Ethan's Website Dev Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }:
+  let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+  in
+    {
+      devShell = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        with pkgs;
+        mkShell {
+          name = "dev shell";
+          buildInputs = [
+              git
+              mdcat
+              nodejs
+              pnpm
+          ];
+        }
+      );
+    };
+}
