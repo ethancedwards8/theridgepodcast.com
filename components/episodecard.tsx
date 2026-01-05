2d9344d763c21f15a741812e60e527265ceb6dda
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-01-04 23:30:35 -0500

    some responsive design fixes ;)
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- components/episodecard.tsx
+++ components/episodecard.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import dayjs from 'dayjs';
 
+import styles from '../styles/episodecard.module.scss';
+
 export default function EpisodeCard({ post }) {
     return (
-        <>
+        <div className={styles.card}>
             <hr/>
             <Link href={'/podcast/' + post.slug} passHref>
                 <h1>{post.frontMatter.title}</h1>
@@ -11,7 +13,7 @@ export default function EpisodeCard({ post }) {
             <h4>{post.frontMatter.author}</h4>
             <h4>{dayjs(post.frontMatter.date).format('MMMM D, YYYY')}</h4>
             <p>{post.frontMatter.description}</p>
-        </>
+        </div>
     )
 }
 
