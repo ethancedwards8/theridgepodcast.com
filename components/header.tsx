45912180e104501ed62bb4a0d77f38d031488d10
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-01-04 20:57:22 -0500

    add logo to cdn
    love love love cdnnnnnnn

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/header.module.scss';
 export default function Header() {
     return (
         <nav className={styles.nav}>
-            <Link href="/"><img src="/ethanandluke/favicon.svg" alt="The Ridge Podcast Logo" /></Link>
+            <Link href="/"><img src="https://media.theridgepodcast.com/TheRidgePodcastLogoECEJLW3000x3000.png" alt="The Ridge Podcast Logo" /></Link>
             <div>
                 <Link href="/"><h2>The Ridge Podcast</h2></Link>
                 <ul>
