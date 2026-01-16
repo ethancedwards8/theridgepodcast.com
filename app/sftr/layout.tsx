e09e5faaa9525c93d097a07bcf3020dbdd5bc80b
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-01-16 14:23:43 -0500

    fix ads

--- app/sftr/layout.tsx
+++ app/sftr/layout.tsx
@@ -1,10 +1,12 @@
+import Script from "next/script";
+
 export default function SFTRLayout({
     children,
 }) {
     // things are expensive :(
     return (
         <>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5292714556730341" crossorigin="anonymous"></script>
+            <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5292714556730341" crossOrigin="anonymous" strategy="afterInteractive" />
 
             <div>
                 {children}
