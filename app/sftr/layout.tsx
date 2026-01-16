25ad961e830cfb15c2583c4e7de3a4a1a424cfdd
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-01-16 14:19:05 -0500

    adds on sftr?... we will see
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

---
+++ app/sftr/layout.tsx
@@ -0,0 +1,14 @@
+export default function SFTRLayout({
+    children,
+}) {
+    // things are expensive :(
+    return (
+        <>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5292714556730341" crossorigin="anonymous"></script>
+
+            <div>
+                {children}
+            </div>
+        </>
+    );
+}
